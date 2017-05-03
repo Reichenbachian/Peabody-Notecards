937258b487ce284ad768a06b2b6addc5446efa25
Reichenbachian <alex@reichenbach.org>
2017-05-03 18:58:54 -0400

    Updating for presentation

--- PeabodyFrontend/dashboard/models.py
+++ PeabodyFrontend/dashboard/models.py
@@ -1,17 +1,22 @@
 from django.db import models
-
+import uuid
 # Create your models here.
 class Entry(models.Model):
     catNumber = models.CharField(max_length=15, blank=True, null=True)
-    siteNumber = models.CharField(max_length=70, blank=True, null=True)
+    siteNumber = models.CharField(max_length=120, blank=True, null=True)
     locality = models.TextField(blank=True, null=True)
-    site = models.CharField(max_length=70, blank=True, null=True)
-    name = models.CharField(max_length=70, blank=True, null=True)
+    site = models.CharField(max_length=120, blank=True, null=True)
+    fileName = models.CharField(max_length=120, blank=True, null=True)
+    name = models.TextField(blank=True, null=True)
     situation = models.TextField(blank=True, null=True)
+    uid=models.CharField(max_length=100, blank=True, unique=True, default=uuid.uuid4)
+    def shortInfo(self):
+        return {"catNumber": self.catNumber, "situation": self.situation, "uuid": self.uid}
     def toDict(self):
     	return {"catNumber": self.catNumber, \
     			"siteNumber": self.siteNumber, \
     			"locality": self.locality, \
     			"site": self.site, \
     			"name": self.name, \
-    			"situation": self.situation}
+    			"situation": self.situation, \
+                "uuid": self.uid}
--- PeabodyFrontend/dashboard/urls.py
+++ PeabodyFrontend/dashboard/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-	url(r'api/getSearchResults/$', views.getSearchResults),
+	url(r'api/$', views.apiCall),
 	url(r'dashboard', views.index, name='index'),
 	url(r'^$', views.index, name='index'),
 ]
