ec6b9c90b23d5b13a5de532dc99058008c0d2023
Reichenbachian <alex@reichenbach.org>
2017-05-03 23:19:22 -0400

    Updating fields in util

--- util/csvToDjango.py
+++ util/csvToDjango.py
@@ -3,12 +3,28 @@ import csv
 with open("/Users/localhost/Desktop/Projects/Working/PeabodyNotecards/test.csv") as f:
     reader = csv.reader(f)
     for row in reader:
-        print(row[3])
         _, created = Entry.objects.get_or_create(
-            catNumber=row[0],
-            siteNumber=row[1],
-            locality=row[2],
-            site=row[3],
-            name=row[4],
-            situation=row[5]
+            catNumber=row[1],
+            siteNumber=row[2],
+            locality=row[3],
+            site=row[4],
+            name=row[5],
+            situation=row[6],
+            accNum=row[7],
+            fileName=row[8]
             )
+
+    # i = 0
+    # for row in reader:
+    #     print("catNumber", row[1])
+    #     print("siteNumber", row[2])
+    #     print("locality", row[3])
+    #     print("site", row[4])
+    #     print("name", row[5])
+    #     print("situation", row[6])
+    #     print("accNum", row[7])
+    #     print("fileName", row[8])
+    #     print("---------------------")
+    #     if i > 3:
+    #         break
+    #     i+=1
