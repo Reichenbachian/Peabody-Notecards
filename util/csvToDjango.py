a5a11ca203b87e936b6d3ce550ac82e4ecf8e679
Reichenbachian <alex@reichenbach.org>
2017-05-04 01:45:08 -0400

    Ready for presentation

--- util/csvToDjango.py
+++ util/csvToDjango.py
@@ -1,17 +1,17 @@
 from dashboard.models import *
 import csv
-with open("/Users/localhost/Desktop/Projects/Working/PeabodyNotecards/test.csv") as f:
+with open("/Users/localhost/Desktop/Projects/Working/PeabodyNotecards/test2.csv") as f:
     reader = csv.reader(f)
     for row in reader:
         _, created = Entry.objects.get_or_create(
-            catNumber=row[1],
-            siteNumber=row[2],
-            locality=row[3],
-            site=row[4],
-            name=row[5],
-            situation=row[6],
-            accNum=row[7],
-            fileName=row[8]
+            catNumber=row[2],
+            siteNumber=row[3],
+            locality=row[4],
+            site=row[5],
+            name=row[6],
+            situation=row[7],
+            accNum=row[8],
+            fileName=row[9]
             )
 
     # i = 0
