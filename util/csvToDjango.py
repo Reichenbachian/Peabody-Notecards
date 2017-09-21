20e527cc03947c7392a58448b4c3f9edf26aebe6
Production server <production_server>
2017-09-21 16:23:44 -0400

    ignoring virtualenv

--- util/csvToDjango.py
+++ util/csvToDjango.py
@@ -1,17 +1,17 @@
 from dashboard.models import *
 import csv
-with open("/Users/localhost/Desktop/Projects/Working/PeabodyNotecards/test2.csv") as f:
+with open("/home/greichenbach/peabody/test3.csv") as f:
     reader = csv.reader(f)
     for row in reader:
         _, created = Entry.objects.get_or_create(
-            catNumber=row[2],
-            siteNumber=row[3],
-            locality=row[4],
-            site=row[5],
-            name=row[6],
-            situation=row[7],
-            accNum=row[8],
-            fileName=row[9]
+            catNumber=str(row[2].encode('ascii', errors='ignore').decode('ascii')),
+            siteNumber=str(row[3].encode('ascii', errors='ignore').decode('ascii')),
+            locality=str(row[4].encode('ascii', errors='ignore').decode('ascii')),
+            site=str(row[5].encode('ascii', errors='ignore').decode('ascii')),
+            name=str(row[6].encode('ascii', errors='ignore').decode('ascii')),
+            situation=str(row[7].encode('ascii', errors='ignore').decode('ascii')),
+            accNum=str(row[8].encode('ascii', errors='ignore').decode('ascii')),
+            fileName=str(row[9].encode('ascii', errors='ignore').decode('ascii'))
             )
 
     # i = 0
